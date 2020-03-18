472dcdf63942fa733efae8430dab4a9767685770
Jonathan Bowen <ew15dro6k216@opayq.net>
2020-03-17 21:11:24 -0400

    Continue HW5

--- HW5-Process-API/HW5-4.c
+++ HW5-Process-API/HW5-4.c
@@ -6,20 +6,23 @@
 
 int main(int argc, char *argv[])
 {
+
   int rc = fork();  // rc: return code
 
   if (rc < 0) {             // error
     fprintf(stderr, "fork failed\n");
     exit(1);
-  // } else if (rc > 0) {
-  //   printf("goodbye\n");
   } else if (rc == 0) {    // child process
-    printf("hello\n");
+    printf("I am child (PID %d).\n", getpid());
+    char *arg_list[2];
+    arg_list[0] = "/bin/ls";
+    arg_list[1] = "";
+    arg_list[2] = '\0';
+    // execl(arg_list[0], arg_list[1], arg_list[2]);
+    execl(arg_list[0], arg_list[1], (char *) NULL);
   } else {                // parent process
     // wait(NULL);
-    // pause();
-    usleep(10 * 1000);
-    printf("goodbye\n");
+    printf("I am parent (PID %d).\n", getpid());
   }
 
   return 0;
